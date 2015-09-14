a357840031ab3a83b6fb0faaa92f16e0f20be884
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2015-09-14 14:21:41 +0200

    attributes added

--- library/src/main/java/com/silverforge/controls/BusyIndicator.java
+++ library/src/main/java/com/silverforge/controls/BusyIndicator.java
@@ -1,26 +1,35 @@
 package com.silverforge.controls;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class BusyIndicator extends View {
-    private static final int POINT_COUNT = 12;
+import com.silverforge.library.R;
 
-    private float layoutCenterX;
-    private float layoutCenterY;
-    private float baseRadius;
-    private float pointRadius;
+public class BusyIndicator extends View {
+    private int pointCount;
+
     private boolean firstLoad = true;
-    private ItemCoordinate[] outerItems = new ItemCoordinate[POINT_COUNT];
+    private ItemCoordinate[] outerItems;
     private Paint outerPaint;
     private Paint singlePaint;
     private ItemCoordinate single;
+
+    private float layoutCenterX;
+    private float layoutCenterY;
+
+    private float baseRadius;
+    private float basePointRadius;
+
     private float singleRadius;
     private float singlePointRadius;
+
+    private byte angleModifier = 1;
 
     public BusyIndicator(Context context) {
         this(context, null);
@@ -33,48 +42,87 @@ public class BusyIndicator extends View {
     public BusyIndicator(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
+        TypedArray attributes
+            = context
+                .getTheme()
+                .obtainStyledAttributes(attrs,
+                                        R.styleable.BusyIndicator,
+                                        defStyleAttr,
+                                        0);
+        int bigPointColor = attributes.getColor(R.styleable.BusyIndicator_bigpoint_color, Color.DKGRAY);
+        int smallPointColor = attributes.getColor(R.styleable.BusyIndicator_smallpoint_color, Color.BLACK);
+        int bigPointCount = attributes.getColor(R.styleable.BusyIndicator_bigpoint_count, Color.BLACK);
+        if (bigPointCount < 4)
+            bigPointCount = 4;
+        if (bigPointCount > 20)
+            bigPointCount = 20;
+
+        pointCount= bigPointCount;
+        outerItems = new ItemCoordinate[pointCount];
+
         outerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        outerPaint.setColor(Color.DKGRAY);
+        outerPaint.setColor(bigPointColor);
 
         singlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        singlePaint.setColor(Color.BLACK);
+        singlePaint.setColor(smallPointColor);
+
+        attributes.recycle();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawARGB(100, 200, 200, 200);
+//        canvas.drawARGB(100, 200, 200, 200);
 
         if (firstLoad) {
             initializePoints();
             firstLoad = false;
         } else {
-            for (int i = 0; i < POINT_COUNT; i++) {
-                float angle = outerItems[i].getAngle();
-                angle += 1;
-                if (angle > 360)
-                    angle = angle - 360;
-
-                ItemCoordinate itemCoordinate = getItemCoordinate(angle, baseRadius);
-                outerItems[i] = itemCoordinate;
-            }
-
-            float singleAngle = single.getAngle();
-            singleAngle -= 1;
-            if (singleAngle < 0)
-                singleAngle = singleAngle + 360;
-
-            single = getItemCoordinate(singleAngle, singleRadius);
-        }
-
-        for (ItemCoordinate item : outerItems) {
-            canvas.drawCircle(item.getX(), item.getY(), pointRadius, outerPaint);
+            calculateMoves();
         }
 
         canvas.drawCircle(single.getX(), single.getY(), singlePointRadius, singlePaint);
+        for (ItemCoordinate item : outerItems) {
+            canvas.drawCircle(item.getX(), item.getY(), item.getRadius(), outerPaint);
+        }
 
         invalidate();
+    }
+
+    private void calculateMoves() {
+        float singleAngle = single.getAngle();
+        singleAngle -= angleModifier;
+        if (singleAngle < 0)
+            singleAngle = singleAngle + 360;
+
+        single = getItemCoordinate(singleAngle, singleRadius);
+
+        for (int i = 0; i < pointCount; i++) {
+            float angle = outerItems[i].getAngle();
+            angle += angleModifier;
+            if (angle > 360)
+                angle = angle - 360;
+
+            ItemCoordinate itemCoordinate = getItemCoordinate(angle, baseRadius, single);
+            outerItems[i] = itemCoordinate;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+
+                if (angleModifier > 1)
+                    angleModifier = 1;
+                else
+                    angleModifier = 2;
+                break;
+        }
+
+        return super.onTouchEvent(event);
     }
 
     private void initializePoints() {
@@ -82,13 +130,13 @@ public class BusyIndicator extends View {
         int width = getWidth();
         baseRadius = height > width ? width /2 : height / 2;
         baseRadius = (float)(baseRadius * 0.75);
-        pointRadius = (float) (baseRadius * 0.15);
+        basePointRadius = (float) (baseRadius * 0.15);
 
         layoutCenterX = getPaddingLeft() + width / 2;
         layoutCenterY = getPaddingTop() + height / 2;
 
-        float slice = 360 / POINT_COUNT;
-        for (int i = 0; i < POINT_COUNT; i++) {
+        float slice = 360 / pointCount;
+        for (int i = 0; i < pointCount; i++) {
             int angle = (int) (slice * i);
             ItemCoordinate itemCoordinate = getItemCoordinate(angle, baseRadius);
             outerItems[i] = itemCoordinate;
@@ -100,6 +148,10 @@ public class BusyIndicator extends View {
     }
 
     protected ItemCoordinate getItemCoordinate(float angleInDegrees, float radius) {
+        return getItemCoordinate(angleInDegrees, radius, null);
+    }
+
+    protected ItemCoordinate getItemCoordinate(float angleInDegrees, float radius, ItemCoordinate affectedItem) {
         ItemCoordinate retValue = new ItemCoordinate();
 
         float x = (float) ((radius * Math.cos(angleInDegrees * Math.PI / 180F)) + layoutCenterX);
@@ -108,6 +160,12 @@ public class BusyIndicator extends View {
         retValue.setX(x);
         retValue.setY(y);
         retValue.setAngle(angleInDegrees);
+        retValue.setRadius(basePointRadius);
+
+//        if (affectedItem != null) {
+//            float abs = Math.abs(affectedItem.getAngle() - retValue.getAngle());
+//            retValue.setRadius((float) (basePointRadius + (abs * 0.20)));
+//        }
 
         return retValue;
     }
--- library/src/main/java/com/silverforge/controls/ItemCoordinate.java
+++ library/src/main/java/com/silverforge/controls/ItemCoordinate.java
@@ -4,6 +4,7 @@ class ItemCoordinate {
     private float x;
     private float y;
     private float angle;
+    private float radius;
 
     public float getX() {
         return x;
@@ -27,5 +28,13 @@ class ItemCoordinate {
 
     public void setAngle(float angle) {
         this.angle = angle;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
     }
 }
