301a4817e6df121c92e1b8e67bf18a1988b2845f
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2015-10-08 10:15:52 +0200

    MultiLoader done

--- app/src/main/java/com/silverforge/busyindicator/MultiLoaderFragment.java
+++ app/src/main/java/com/silverforge/busyindicator/MultiLoaderFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,22 +14,23 @@ import java.util.Random;
 
 public class MultiLoaderFragment extends Fragment {
     private BusyIndicator multiBusyIndicator1;
+    private BusyIndicator multiBusyIndicator2;
+    private BusyIndicator multiBusyIndicator3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_multi_loader, container, false);
         multiBusyIndicator1 = (BusyIndicator) view.findViewById(R.id.finiteMultiBusyIndicator1);
+        multiBusyIndicator2 = (BusyIndicator) view.findViewById(R.id.finiteMultiBusyIndicator2);
+        multiBusyIndicator3 = (BusyIndicator) view.findViewById(R.id.finiteMultiBusyIndicator3);
 
         multiBusyIndicator1.setMaxValue(83);
-        multiBusyIndicator1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    new FiniteMultiBusyIndicatorAsync1().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-                }
-                return true;
-            }
-        });
+        multiBusyIndicator2.setMaxValue(530);
+        multiBusyIndicator3.setMaxValue(962);
+
+        new FiniteMultiBusyIndicatorAsync1().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        new FiniteMultiBusyIndicatorAsync2().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        new FiniteMultiBusyIndicatorAsync3().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         return view;
     }
 
@@ -65,6 +65,82 @@ public class MultiLoaderFragment extends Fragment {
                 @Override
                 public void run() {
                     multiBusyIndicator1.setValue(83);
+                }
+            });
+
+            return null;
+        }
+    }
+
+    class FiniteMultiBusyIndicatorAsync2 extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+
+            final Random rand = new Random();
+            int i = 0;
+
+            FragmentActivity activity = getActivity();
+
+            while (i < 531) {
+                final int a = i;
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                    multiBusyIndicator2.setValue(a);
+                    }
+                });
+
+                i = i + rand.nextInt(60) + 1;
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    multiBusyIndicator2.setValue(530);
+                }
+            });
+
+            return null;
+        }
+    }
+
+    class FiniteMultiBusyIndicatorAsync3 extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+
+            final Random rand = new Random();
+            int i = 0;
+
+            FragmentActivity activity = getActivity();
+
+            while (i < 963) {
+                final int a = i;
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                    multiBusyIndicator3.setValue(a);
+                    }
+                });
+
+                i = i + rand.nextInt(230) + 1;
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    multiBusyIndicator3.setValue(962);
                 }
             });
 
